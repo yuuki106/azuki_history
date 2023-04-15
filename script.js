d913577a148eb6a49ad4d6de68ac4a2b93de1571
yuuki106 <117831203+yuuki106@users.noreply.github.com>
2023-04-15 09:31:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,17 +17,33 @@ $(function(){
     });
 
 
-    $('.open-btn').click(function(){
+    $('#open-btn1').click(function(){
 
-        var $active=$(this).find('.histories');
+        var $active=$('#histories1');
 
         if($active.hasClass('open')){
             $active.removeClass('open')
-            $active.slideUp();
+            $active.slideUp('fast');
             $(this).find('.history-span').text('+');
         }else{
             $active.addClass('open')
-            $active.slideDown();
+            $active.slideDown('fast');
+            $(this).find('.history-span').text('-');
+        };
+
+    });
+
+    $('#open-btn2').click(function(){
+
+        var $active=$('#histories2');
+
+        if($active.hasClass('open')){
+            $active.removeClass('open')
+            $active.slideUp('fast');
+            $(this).find('.history-span').text('+');
+        }else{
+            $active.addClass('open')
+            $active.slideDown('fast');
             $(this).find('.history-span').text('-');
         };
 
